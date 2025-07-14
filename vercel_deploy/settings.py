1ff59b9e5d84c417ec01c8e48856f08584fc5e82
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-07-14 10:38:24 +0700

    config vercel deployment

--- vercel_deploy/settings.py
+++ vercel_deploy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
