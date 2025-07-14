3a860d0b656d1b920602dc9ebf1374ee1fff62ee
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-07-14 11:25:54 +0700

    edit

--- vercel_deploy/settings.py
+++ vercel_deploy/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'statics')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
