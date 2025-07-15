0f73c2ff7415d694e1bb23d5b42456e7ae702c79
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-07-15 15:16:40 +0700

    C

--- vercel_deploy/view.py
+++ vercel_deploy/view.py
@@ -1,2 +1,17 @@
-from django.urls import render
+from django.shortcuts import render
+from httpx import request
 
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+                  
+                
--- webpage/views.py
+++ webpage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from httpx import request
+
 
 
 
